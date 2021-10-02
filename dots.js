3edc18ada45e95bf1ad698dbd9ae0c492f578e1d
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-10-01 23:37:40 -0500

    dots

---
+++ dots.js
@@ -0,0 +1,51 @@
+const grid = document.querySelector('.grid');
+const dotSize = 1;
+const padding = 40;
+const sizes = [0, 0, 0, 0, 1.5, 1.5, 1.5, 1.5, 3, 4, 5];
+let animationReq;
+let increment = 0;
+let dots = [];
+window.addEventListener('resize', createGrid);
+
+createGrid();
+
+function createGrid(event) {
+    clearGrid();
+
+    const gridR = grid.getBoundingClientRect();
+    const width = (gridR.width - padding * 2) / (padding - dotSize / 2);
+    const height = (gridR.height - padding * 2) / (padding - dotSize / 2);
+
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            const dot = document.createElement('div');
+            dot.className = 'dot';
+            dot.style.top = j * padding + padding + 'px';
+            dot.style.left = i * padding + padding + 'px';
+            grid.appendChild(dot);
+            dots.push(dot);
+        }
+    }
+
+    animationReq = window.requestAnimationFrame(animateGrid);
+}
+
+function clearGrid() {
+    window.cancelAnimationFrame(animationReq);
+    while (grid.firstChild) {grid.removeChild(grid.firstChild);}
+    dots = [];
+}
+
+
+function animateGrid() {
+    const dot = dots[Math.floor(Math.random() * dots.length)];
+    const scale = sizes[Math.floor(Math.random() * sizes.length)];
+    const timeoutTime = Math.floor(Math.random() * (5000 - 10 + 1)) + 10;
+    dot.style.transform = `scale(${scale})`;
+    const st1 = setTimeout(() => {
+        dot.style.transform = `scale(1)`;
+        clearTimeout(st1);
+    }, 600);
+
+    animationReq = window.requestAnimationFrame(animateGrid);
+}
