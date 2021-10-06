9a36c88ddae61465d1aa23e086fae370ce4ab07b
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-10-06 02:34:23 -0500

    color movement

--- dots.js
+++ dots.js
@@ -124,14 +124,14 @@ function setTransition(event) {
 
 /////////// typewriter /////////////////////
 
-const words = ["Welcome...", "to", "The", "集 合 的 無 意 識"]
+const words = ["集 合 的 無 意 識","Welcome...", "to", "The", "集 合 的 無 意 識"]
 
 let cursor = gsap.to('.cursor', {opacity:0, ease: "power2.inOut", repeat:-1})
 let masterTl = gsap.timeline({repeat: -1}).pause()
 let boxTl = gsap.timeline()
 
 boxTl.to('.box', {duration:1, width:"17vw", delay: 0.5, ease: "power4.inOut"})
-    .from('.hi', {duration:1, y:"7vw", ease: "power3.out"})
+    .from('.hi', {duration:1, y:"-101vw", ease: "power3.out"})
     .to('.box', {duration:1, height:"7vw", ease: "elastic.out", onComplete: () => masterTl.play() })
     .to('.box', {duration:2, autoAlpha:0.7, yoyo: true, repeat: -1, ease:"rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})"})
 words.forEach(word => {
