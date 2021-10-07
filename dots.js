ced784a9dc6873e6e6d43b98e8c86e145aec195e
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-10-07 09:40:47 -0500

    noise

--- dots.js
+++ dots.js
@@ -68,7 +68,7 @@ function animateGrid() {
 
 const tiltEffectSettings = {
     max: 15, // max tilt rotation (degrees (deg))
-    perspective: 7000, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
+    perspective: 10000, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
     scale: 1, // transform scale - 2 = 200%, 1.5 = 150%, etc..
     speed: 1500, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
     easing: "cubic-bezier(.03,.98,.52,.99)" // easing (transition-timing-function) of the enter/exit transition
@@ -151,6 +151,101 @@ words.forEach(word => {
         masterTl.add(tl)
 
     })
+
+
+
+
+//////////////// NOISE////////////
+
+const noise = () => {
+    let canvas, ctx;
+
+    let wWidth, wHeight;
+
+    let noiseData = [];
+    let frame = 0;
+
+    let loopTimeout;
+
+
+    // Create Noise
+    const createNoise = () => {
+        const idata = ctx.createImageData(wWidth, wHeight);
+        const buffer32 = new Uint32Array(idata.data.buffer);
+        const len = buffer32.length;
+
+        for (let i = 0; i < len; i++) {
+            if (Math.random() < 0.5) {
+                buffer32[i] = 0xff000000;
+            }
+        }
+
+        noiseData.push(idata);
+    };
+
+
+    // Play Noise
+    const paintNoise = () => {
+        if (frame === 9) {
+            frame = 0;
+        } else {
+            frame++;
+        }
+
+        ctx.putImageData(noiseData[frame], 0, 0);
+    };
+
+
+    // Loop
+    const loop = () => {
+        paintNoise(frame);
+
+        loopTimeout = window.setTimeout(() => {
+            window.requestAnimationFrame(loop);
+        }, (1000 / 25));
+    };
+
+
+    // Setup
+    const setup = () => {
+        wWidth = window.innerWidth;
+        wHeight = window.innerHeight;
+
+        canvas.width = wWidth;
+        canvas.height = wHeight;
+
+        for (let i = 0; i < 10; i++) {
+            createNoise();
+        }
+
+        loop();
+    };
+
+
+    // Reset
+    let resizeThrottle;
+    const reset = () => {
+        window.addEventListener('resize', () => {
+            window.clearTimeout(resizeThrottle);
+
+            resizeThrottle = window.setTimeout(() => {
+                window.clearTimeout(loopTimeout);
+                setup();
+            }, 200);
+        }, false);
+    };
+
+
+    // Init
+    const init = (() => {
+        canvas = document.getElementById('noise');
+        ctx = canvas.getContext('2d');
+
+        setup();
+    })();
+};
+
+noise();
 
 
 
